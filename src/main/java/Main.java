19197fbcf5c020684c3b5aabebb273e3ad467859
1Neikowo <71148894+1Neikowo@users.noreply.github.com>
2023-10-11 16:29:36 -0300

    c1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola1");
+        System.out.println(restar(1,2));
+    }
+    public static int restar(int a, int b){
+        return a-b;
     }
 }

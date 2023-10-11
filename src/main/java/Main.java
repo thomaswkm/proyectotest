3c232e6f44e7f41614ef9a8daf5197dd4ec3e074
thomas <t.kinderman01@ufromail.cl>
2023-10-11 16:27:14 -0300

    c2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola1");
+        System.out.println(sumar(1,2));
+    }
+
+    public static int sumar(int a, int b){
+        return a+b;
     }
 }
